25574d5216e98bd5916c0ce1f612b849dc7d2ddd
giovannivigilante7-gif <giovannivigilante7@gmail.com>
2026-07-15 00:49:50 +0200

    Pubblica Italia 2027 versione 30

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='italia-2027-v30';
+const ASSETS=['./','./index.html','./404.html','./manifest.webmanifest','./privacy.html','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{let copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
